c153e9570cc4b7ef91c28a97b2a93b88101c68a6
Pedro Henrique <76178088+pedrohenrique11@users.noreply.github.com>
2023-06-26 03:40:18 +0000

    feat: add description for BMI result

--- src/BMI/bmi.js
+++ src/BMI/bmi.js
@@ -3,8 +3,17 @@ const weightInput = document.querySelector('#inputWeight')
 const heightInput = document.querySelector('#inputHeight')
 
 const form = document.querySelector('form')
-
-const resultContainer = document.querySelector('#resultContainer')
+const sectionResult = {
+    section: document.querySelector('#resultModal'),
+    result: document.querySelector('#result'),
+    description: document.querySelector('#description'),
+    open() {
+        sectionResult.section.classList.remove('hide')
+    },
+    close() {
+        sectionResult.section.classList.add('hide')
+    }
+}
 
 form.onsubmit = e => {
     e.preventDefault();
@@ -13,13 +22,30 @@ form.onsubmit = e => {
     let height = heightInput.value;
 
     const result = calculateBMI(weight, height)
+    const description = setDescription(result)
 
-    resultContainer.innerHTML = Number(result);
+    sectionResult.result.innerHTML = `Your BMI is ${result}`;
+    sectionResult.description.innerHTML = description;
+    sectionResult.open()
 }
 
 function calculateBMI(weight, height) {
     return (weight / (height/ 100)**2).toFixed(2);
 };
-
+function setDescription(BMI) {
+    if( BMI <= 18.5 ) {
+        return 'You are underweight!'
+    }
+    else if( BMI <= 24.9 ) {
+        return 'You are in healthy weight!'
+    }
+    else if( BMI <= 29.9 ) {
+        return 'You are overweight!'
+    }
+    else if( BMI > 30 && BMI < 39.9 ) {
+        return 'You are in obesity! Please, be careful with your health :)'
+    }
+    else return 'You are in class 3 obesity! Please, be careful with your health :)'
+}
 
 module.exports = calculateBMI;
